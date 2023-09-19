3a06152b1a30ba874c73b2658c27cc3ed8f233a3
sean <sean2337@naver.com>
2023-09-19 14:00:16 +0900

    Node.js-36.App 제작-글수정-파일병 변경, 내용저장

--- main.js
+++ main.js
@@ -130,6 +130,24 @@ var app = http.createServer(function (request, response) {
       });
     });
     ///////
+  } else if (pathName === '/update_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body = body + data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+
+      fs.rename(`data/${id}`, `data/${title}`, function (err) {
+        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+          response.writeHead(302, { Location: `/?id=${title}` });
+          response.end('success');
+        });
+      });
+    });
   } else {
     response.writeHead(404, { 'Content-Type': 'text/plain' });
     response.end('Not Found');
