87eb2c7d6dcf162cbe93722aa9764221a02c66f1
sean <sean2337@naver.com>
2023-09-19 14:11:37 +0900

    Node.js-38.App 제작-글삭제 기능 완성

--- main.js
+++ main.js
@@ -28,6 +28,10 @@ function sendPage(response, title, description) {
         </ol>
         <a href="/create">create</a>
         <a href="/update?id=${title}">update</a>
+        <form action="delete_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <input type="submit" value="delete">
+        </form>
         <h2>${title}</h2>
         <p>${description}</p>
       </body>
@@ -146,6 +150,19 @@ var app = http.createServer(function (request, response) {
           response.writeHead(302, { Location: `/?id=${title}` });
           response.end('success');
         });
+      });
+    });
+  } else if (pathName === '/delete_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body = body + data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function (err) {
+        response.writeHead(302, { Location: `/` });
+        response.end('success');
       });
     });
   } else {
